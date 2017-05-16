cb3632ccb5a742a5ce615c0ad73bc8bd30d743a7
mayankjhawar <mayank.jhawar100@gmail.com>
2017-05-16 19:03:34 +0530

    DEx.com

---
+++ scripts/default.js
@@ -0,0 +1,6 @@
+$(function(){
+	$("#form").hide();
+	$("#button_apt").click(function(){
+		$("#form").show();
+	});
+});
---
+++ scripts/experiment.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+	$('#change').click(function (){
+	 	$('#slide').slideUp('slow')
+		
+
+
+
+   });
+
+});
